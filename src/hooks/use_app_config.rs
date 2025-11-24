2a15504cd402fadb31752a9aad9fb679a3195ad3
The Mist <mist.zzh@gmail.com>
2025-11-24 21:36:11 +0800

    [Feat] resolve conflicts

--- src/hooks/use_app_config.rs
+++
@@ -1,8 +0,0 @@
-use crate::utils::AppConfigService;
-use yew::prelude::*;
-
-/// Hook for using AppConfig in Yew components
-#[hook]
-pub fn use_app_config() -> UseStateHandle<AppConfigService> {
-    use_state(|| AppConfigService::new())
-}
