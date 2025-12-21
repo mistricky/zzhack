2bafaec68abf3cde179780576e92a73d6175de22
The Mist <mist.zzh@gmail.com>
2025-12-21 16:45:35 +0800

    [Chore] remove theme switch button temporarily

--- app/src/components/terminal_window.rs
+++ app/src/components/terminal_window.rs
@@ -1,10 +1,6 @@
 use crate::components::{HeaderBar, OutputLog, PromptLine};
 use crate::types::TermLine;
-use web_sys::window;
 use yew::prelude::*;
-
-const DARK_MODE_ICON: &str = include_str!("../icons/dark_mode.svg");
-const LIGHT_MODE_ICON: &str = include_str!("../icons/light_mode.svg");
 
 #[derive(Properties, PartialEq)]
 pub struct TerminalWindowProps {
@@ -18,35 +14,6 @@ pub struct TerminalWindowProps {
 
 #[function_component(TerminalWindow)]
 pub fn terminal_window(props: &TerminalWindowProps) -> Html {
-    let is_dark_icon = use_state(|| true);
-
-    let toggle_icon = {
-        let is_dark_icon = is_dark_icon.clone();
-        Callback::from(move |_| is_dark_icon.set(!*is_dark_icon))
-    };
-
-    {
-        let is_dark_icon = is_dark_icon.clone();
-        use_effect_with(is_dark_icon, move |is_dark| {
-            if let Some(document) = window().and_then(|w| w.document()) {
-                if let Some(root) = document.document_element() {
-                    let _ = if **is_dark {
-                        root.class_list().add_1("theme-dark")
-                    } else {
-                        root.class_list().remove_1("theme-dark")
-                    };
-                }
-            }
-            || ()
-        });
-    }
-
-    let icon_svg = if *is_dark_icon {
-        DARK_MODE_ICON
-    } else {
-        LIGHT_MODE_ICON
-    };
-    let icon = Html::from_html_unchecked(AttrValue::from(icon_svg));
     let body = html! {
         <>
             <OutputLog lines={props.lines.clone()} />
@@ -61,15 +28,6 @@ pub fn terminal_window(props: &TerminalWindowProps) -> Html {
 
     html! {
         <div class="min-h-[100svh] bg-page text-text flex items-start sm:items-center justify-center px-4 pt-16 pb-6 sm:p-6 overflow-y-auto">
-            <button
-                type="button"
-                class="fixed top-4 right-4 sm:top-6 sm:right-6 h-9 w-9 sm:h-10 sm:w-10 flex items-center justify-center rounded-full text-border hover:bg-hover transition-colors"
-                aria-label="Toggle theme"
-                onclick={toggle_icon}
-            >
-                { icon }
-            </button>
-
             {
                 if !props.show_window {
                     html! {
